b80883f6ebfb6196d3234d08466648e742faff5a
Timo David <raghu.doppalapudi@gmail.com>
2013-02-10 03:25:41 -0800

    updated POM snippet with CDH repo and added wordcount classes

---
+++ src/main/java/com/cloudack/mr/driver/WordCountDriver.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package com.cloudack.mr.driver;
+
+import java.util.Map;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+import com.cloudack.mr.mapper.WordCountMapper;
+import com.cloudack.mr.reducer.WordCountReducer;
+
+/**
+ * @author pudi
+ * 
+ */
+public class WordCountDriver {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		//System.setProperty("HADOOP_USER_NAME", "root");
+
+
+		Job job = new Job(conf, "wordcount");
+
+		job.setJarByClass(WordCountDriver.class);
+
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+
+		job.setMapperClass(WordCountMapper.class);
+		job.setReducerClass(WordCountReducer.class);
+
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+		job.waitForCompletion(true);
+	}
+
+}
